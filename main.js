336a019d63fa8cbebea0988e447f3f2a09308a55
nanirajesh.g <nanirajesh.g@gmail.com>
2017-02-20 21:34:45 +0530

    Added some Styles

--- main.js
+++ main.js
@@ -1,56 +1,36 @@
-(function(){
-'use strict';
 var app=angular.module('MainApp',[]);
-
-app.service('MovieDataService',MovieDataService);
-app.controller('SearchController',['$scope',SearchController]);
-
-app.controller('FilterController',['$scope',FilterController]);
-
-app.controller('DisplayMoviesController',['MovieDataService',function(MovieDataService){
-    var promise=MovieDataService.getData();
-    promise.then(function(response){
-        console.log(response.data);
-    },function(response){
-        console.log(response.errormessage);
-    });
-}]);
-
-app.directive('movieItem',MovieItem);
-
-
-
+app.controller('DisplayMoviesController',DisplayMoviesController);
+app.controller('SearchController',SearchController);
+app.controller('FilterController',FilterController);
+app.service('DataService',DataService);
+app.directive('movieItem',movieItem);
+function DataService($http){
+   var service=this;
+   this.getData=function(){
+      var promise=$http({
+          url:'https://davids-restaurant.herokuapp.com/menu_items.json'
+      });
+      return promise;
+   };
+}
 
 function SearchController(){
-
+    
 }
 
 function FilterController(){
 
 }
-//DisplayMoviesController.$inject=['MovieDataService'];
 
-
-MovieDataService.$inject=['$scope','$http'];
-function MovieDataService($scope,$http){  
-    
- this.getData = function(){
-    var data=$http({
-        method:'GET',
-        url:'https://davids-restaurant.herokuapp.com/menu_items.json'
-    });
-   return data;
-    };
+function DisplayMoviesController($scope,DataService){
+   var promise=DataService.getData();
+   promise.then(function(response){
+      $scope.menuitems=response.data.menu_items;
+   });
 }
-
-
-function MovieItem(){
-
+function movieItem(){
     var ddo={
-        templateUrl:"movieitem.html"
-    };
-
+        templateUrl:'movieitem.html'
+    }
     return ddo;
 }
-
-})();
